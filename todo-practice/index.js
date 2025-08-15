12844cad037ee72c5b2ff100409a698c6f15cdca
Nooruddin Md Afsar <nooruddin.mdafsar.it25@heritageit.edu.in>
2025-08-15 15:23:10 +0530

    update todo, now use better approach to create elements

--- todo-practice/index.js
+++ todo-practice/index.js
@@ -1,31 +1,49 @@
 let count = 0;
 
-function addTask(){
+function addTask() {
     const inputElement = document.querySelector('input');
     const value = inputElement.value;
 
     const newDivElement = document.createElement('div');
-    newDivElement.setAttribute('id', 'todo-'+ count)
-    newDivElement.innerHTML = `<div>${value}</div>
-    <button class="edit" onClick="editTodo(${count})">Edit</button>
-    <button class="delete" onClick="deleteTodo(${count})">Delete</button>`;
+    newDivElement.setAttribute('id', 'todo-' + count)
+    // newDivElement.innerHTML = `<div>${value}</div>
+    // <button class="edit" onClick="editTodo(${count})">Edit</button>
+    // <button class="delete" onClick="deleteTodo(${count})">Delete</button>`;
+    const spanElement = document.createElement('span');
+    spanElement.textContent = value;
+
+    const editButton = document.createElement('button');
+    editButton.setAttribute('class', 'edit');
+    editButton.setAttribute('onClick', `editTodo(${count})`);
+    editButton.textContent = 'Edit';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.setAttribute('class', 'delete');
+    deleteButton.setAttribute('onClick', `deleteTodo(${count})`);
+    deleteButton.textContent = 'Delete';
+
+    newDivElement.appendChild(spanElement);
+    newDivElement.appendChild(editButton);
+    newDivElement.appendChild(deleteButton);
     document.querySelector('body').appendChild(newDivElement);
     count++;
 }
 
-function editTodo(index){
+function editTodo(index) {
     const todoElement = document.getElementById(`todo-${index}`);
     if (todoElement) {
-        const newValue = prompt("Edit your todo item:", todoElement.querySelector('div').textContent);
+        const spanElement = todoElement.querySelector('span');
+        const newValue = prompt("Edit your todo item:", spanElement.textContent);
         if (newValue !== null) {
-            todoElement.querySelector('div').textContent = newValue;
+            spanElement.textContent = newValue;
         }
     } else {
         console.error('Todo item not found');
     }
 }
 
-function deleteTodo(index){
+
+function deleteTodo(index) {
     const todoElement = document.getElementById(`todo-${index}`);
     if (todoElement) {
         todoElement.parentNode.removeChild(todoElement);
